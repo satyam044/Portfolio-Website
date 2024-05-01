7566d17dbf944d428f850312b7d310ae1921b6cd
satyam <mishrasaryam9890@gmail.com>
2024-05-01 22:11:50 +0530

    responsiveness added

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+function hamburger(){
+    var logo = document.querySelector(".navbar a");
+    var resbtn = document.querySelector(".navbtn button");
+    var hamburger = document.getElementById("hamburger");
+    var hamelems = document.querySelector(".hamelems");
+
+    let clicked = false;
+    
+    hamburger.addEventListener("click",()=>{
+        if (!clicked) {
+            hamelems.style.top = "0"
+            logo.style.color = "#fff"
+            resbtn.style.display = "none"
+            clicked = true;
+        } else{
+            hamelems.style.top = "-60vh"
+            logo.style.color = "#313e4a"
+            resbtn.style.display = "block"
+            clicked = false;
+        }
+    })
+}
+hamburger()
+
 function sendMail() {
     var params = {
         from_name : document.getElementById("name").value,
